02d1b839ea97073af06e9d06a51c08c4b6cd04ae
sharifulislamudoy <surifroton301@gmail.com>
2025-08-29 17:38:15 +0600

    yes

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,10 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientLayout from "./components/ClientLayout";
 
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -23,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo.png"></link>
+        <link rel="icon" href="/logo.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import FounderSection from "./components/FounderSection";
 import HeroSection from "./components/HeroSection";
 import PerformanceAnalytics from "./components/PerformanceAnalytics";
 
-
 export default function Home() {
   return (
     <div>
